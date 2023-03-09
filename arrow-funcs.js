11542606c678a620e8cdce825ddf682380add449
Juliet Stunkel <rstunkel97@gmail.com>
2023-03-08 21:40:21 -0600

    I have been converted to the glory of arrow functions

---
+++ arrow-funcs.js
@@ -0,0 +1,28 @@
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+
+//Refactor the above code to use two arrow functions. Turn it into a one-liner.
+
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions:
+//Replace ALL functions with arrow functions:
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+//Refactored code:
+const squareAndFindEvens = numbers => {
+    const squares = numbers.map(num => num ** 2);
+    const evens = squares.filter(square => square % 2 === 0);
+    return evens;
+};
